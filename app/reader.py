0ddba684ea7e8c2c5cf31136375276070f27fafb
arsenalnox <nohtimore@gmail.com>
2022-03-29 14:09:18 +0500

    - Рабочий импорт в бд из структурированных папок

--- app/reader.py
+++ app/reader.py
@@ -3,43 +3,58 @@
 """
 
 import os
-from app.classes import School
+import pprint as pp
+
+#pp = pprint.PrettyPrinter(indent=5)
 
 def read_root_data()->list:
-	"""
-		Возращает содержимое корня папки data_in
-	"""
-	in_folder_names = os.listdir('data_in/')
-	return in_folder_names
+    """
+        Возращает содержимое корня папки data_in
+    """
+    in_folder_names = os.listdir('data_in/')
+    return in_folder_names
 
 
-def read_schools_contents(school_list:list)->list[School]:
-	"""
-	
-	"""
-	
-	schools = []
+def read_schools_contents(school_list:list)->list:
+    """
+    
+    """
+    
+    schools = []
 
-	for school in school_list:
-		
-		school_content = School(f'data_in/{school}')
+    for school in school_list:
 
-		work_dir = f'data_in/{school}'
+        work_dir = f'data_in/{school}'
 
-		school_dirs  = []
-		school_files = []
+        school_name = school
 
-		for root, dirs, files in os.walk(f'data_in/{school}'):
-			dirs = [ name for name in os.listdir(work_dir) if os.path.isdir(os.path.join(work_dir, name)) ]
-			school_dirs.append(dirs)
-			school_files.append(files)
+        school_dirs  = os.listdir(work_dir)
 
-		school_content.set_dirs(dirs).set_files(files)
+        school_categories = []
 
-		schools.append(school_content)
+        for subdir in school_dirs:
+            subdir_name = subdir
+            subdir_files = []
 
-	return schools
+            if os.path.isdir(f'data_in/{school}/{subdir}/До'):
+                for category_content in os.listdir(f'data_in/{school}/{subdir}/До'):
+                    subdir_files.append(category_content)
 
+            elif os.path.isdir(f"data_in/{school}/{subdir}"):
+                for category_content in os.listdir(f'data_in/{school}/{subdir}'):
+                    subdir_files.append(category_content)
 
-def get_schools()->list[School]:
-	return read_schools_contents(read_root_data())
+            school_categories.append({
+                "subdir_name": subdir,
+                "files": subdir_files
+                })
+        schools.append({
+            "name": school,
+            "cats": school_categories
+            })
+
+    return schools
+
+def read_data():
+    return read_schools_contents(read_root_data())
+
--- main.py
+++ main.py
@@ -1,20 +1,170 @@
+from random import randint
 from app import parser as pr 
 from app import reader as rd
-from typing_extensions import Self
 
-schools_without_foldres = 0
-schools_with_foldres    = 0
+import pprint
+import os 
+import mysql.connector
+import shutil 
 
-for school in rd.get_schools():
 
-	if len(school.dirs) == 0:
-		#print(f'School {school.root_dir} has not folders...')
-		schools_without_foldres += 1 
-	else: 
-		print(school)
-		schools_with_foldres += 1
+sqlConnection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='root',
+        database='min'
+        )
 
-print(f"""
-	Schools without folders: {schools_without_foldres}
-	Schools with folders: {schools_with_foldres}
-""")
+cursor = sqlConnection.cursor()
+
+def is_school_insered(school_name):
+    sql = f"""
+    SELECT 
+        * 
+    FROM educational
+    WHERE `name` = '{school_name}'
+    """
+    cursor.execute(sql)
+
+    result = cursor.fetchall()
+    return result
+
+
+def get_all_munipals_list():
+    sql = """
+    SELECT 
+        *
+    FROM munipal 
+    """
+    cursor.execute(sql)
+
+    return cursor.fetchall()
+
+
+def insert_new_school(school_name):
+    sql = f"""
+    INSERT INTO educational(
+            `name`,
+            `name_full`,
+            `address`,
+            `region`,
+            `project_capacity`,
+            `factial_capacity`
+            )
+    VALUES (
+            '{school_name}',
+            '{school_name}',
+            'г. Оренбург ул. X/Y',
+            '36',
+            '0',
+            '0'
+            )
+    """
+
+    cursor.execute(sql)
+    sqlConnection.commit()
+    return cursor.lastrowid
+
+
+def get_school_id(school_name):
+    sql = f"""
+    SELECT 
+        * 
+    FROM educational
+    WHERE name LIKE "%{school_name}%"
+    """
+    cursor.execute(sql)
+    return cursor.fetchall()[0]
+
+
+def insert_school_photo(school_id, category_id, photo_path):
+    sql = f"""
+    INSERT INTO photos_edu(
+            `schid`,
+            `category`,
+            `path`,
+            `thumb`
+            )
+    VALUES (
+            {school_id},
+            {category_id},
+            '{photo_path}',
+            '{photo_path}'
+            )
+    """
+    cursor.execute(sql)
+    sqlConnection.commit()
+    return cursor.fetchall()
+
+
+def get_category_id_by_name(cat_name):
+    sql = f"""
+    SELECT 
+        *
+    FROM photo_categories 
+    WHERE name LIKE "%{cat_name}%"
+    """
+    cursor.execute(sql)
+    return cursor.fetchall()
+
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+#Check if input and data folders exist 
+if not os.path.exists('data_in'):
+    os.mkdir('data_in')
+
+if not os.path.exists('data_out'):
+    os.mkdir('data_out')
+
+schools = rd.read_data()
+
+all_categories = []
+
+for school in schools:
+    print(school['name'], end=' ')
+    isInserted = is_school_insered(school['name'])
+    if not isInserted:
+        print(f'Inserting school {school["name"]}')
+        school_id = insert_new_school(school['name'])
+
+    else: 
+        school_id = get_school_id(school['name'])[0]
+            
+    if not os.path.exists(f'/srv/http/projects/ministery/img/{school_id}'):
+        try: 
+            os.mkdir(f'/srv/http/projects/ministery/img/{school_id}')
+        except Exception as err:
+            print(err)
+            exit()
+
+    #print(f"\n   {school['name']}")
+    for cat in school['cats']:
+        try:
+            category_id = get_category_id_by_name(cat['subdir_name'])[0][0]
+        except Exception as err:
+            category_id = 16
+    
+
+        if cat['subdir_name'] not in all_categories:
+            all_categories.append(cat['subdir_name'])
+
+        #pprint.pprint(cat['subdir_name'])
+        for photo in cat['files']:
+            photo_ext = str(photo).split('.')[-1]
+            photo_path_to = f"/home/vladislav/programming/python/school_image_parser/data_in/{school['name']}/{cat['subdir_name']}/До"
+            photo_new_name = f"{school_id}-{randint(1,100)}-{randint(1,100)}-{randint(1,10)}.{photo_ext}"
+            file_path_current = f"{photo_path_to}/{photo}"
+            
+            try:
+                shutil.copy(file_path_current, f"/srv/http/projects/ministery/img/{school_id}/{photo_new_name}")
+            except Exception as err:
+                print(err)
+            print('.',end='')
+            insert_school_photo(school_id, category_id, f"img/{school_id}/{photo_new_name}")
+
+    print('')
+
+#print(all_categories)
+#print(len(all_categories))
